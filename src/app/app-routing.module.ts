e5021b973407961ed3d9efc4aa07ef703b513986
Luis del Moral Martínez <ldmoral1987>
2020-10-12 17:43:57 +0200

    Creación del repositorio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { OktaCallbackComponent, OktaAuthGuard } from '@okta/okta-angular';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'timeline',
+    component: TimelineComponent,
+    canActivate: [OktaAuthGuard]
+  },
+  { path: 'implicit/callback', component: OktaCallbackComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
